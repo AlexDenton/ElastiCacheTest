824ca7a766fd711eef6310ef40a031080bfaf09a
Alex Denton <alex.denton.dev@gmail.com>
2018-04-18 20:31:42 -0400

    Refactored into a RequestSimulator

--- ElastiCacheTest/Program.cs
+++ ElastiCacheTest/Program.cs
@@ -14,35 +14,20 @@ namespace ElastiCacheTest
 
         static void Main(string[] args)
         {
-            //_Configuration = new ConfigurationBuilder()
-            //    .SetBasePath(Directory.GetCurrentDirectory())
-            //    .AddJsonFile("appSettings.json")
-            //    .Build();
+            _Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appSettings.json")
+                .Build();
 
             Run().Wait();
         }
 
         private static async Task Run()
         {
-            var redisEndpoints = new List<string>
-            {
-                "dentoncache-001.zbumom.0001.use2.cache.amazonaws.com:6379",
-                "dentoncache-002.zbumom.0001.use2.cache.amazonaws.com:6379",
-                "dentoncache-003.zbumom.0001.use2.cache.amazonaws.com:6379"
-            };
+            var requestSimulator = new RequestSimulator();
+            var requestSimulatorTask = requestSimulator.Run();
 
-            var configurationOptions = new ConfigurationOptions();
-            redisEndpoints.ForEach(configurationOptions.EndPoints.Add);
-            configurationOptions.KeepAlive = 180;
-            configurationOptions.ConfigCheckSeconds = 30;
-            configurationOptions.AbortOnConnectFail = false;
-
-            var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(configurationOptions);
-            var database = connectionMultiplexer.GetDatabase(0);
-            var setResult = database.StringSet("test-key", "test-value");
-            Console.WriteLine(setResult);
-            var getResult = database.StringGet("test-key");
-            Console.WriteLine(getResult.ToString());
+            await requestSimulatorTask;
         }
     }
 }
---
+++ ElastiCacheTest/RequestSimulator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace ElastiCacheTest
+{
+    public class RequestSimulator
+    {
+        private readonly IDatabase _RedisDatabase;
+
+        public RequestSimulator()
+        {
+            _RedisDatabase = GetRedisDatabaseConnection().Result;
+        }
+
+        private async Task<IDatabase> GetRedisDatabaseConnection()
+        {
+            var redisEndpoints = new List<string>
+            {
+                "dentoncache-001.zbumom.0001.use2.cache.amazonaws.com:6379",
+                "dentoncache-002.zbumom.0001.use2.cache.amazonaws.com:6379",
+                "dentoncache-003.zbumom.0001.use2.cache.amazonaws.com:6379"
+            };
+
+            var configurationOptions = new ConfigurationOptions();
+            redisEndpoints.ForEach(configurationOptions.EndPoints.Add);
+            configurationOptions.KeepAlive = 180;
+            configurationOptions.ConfigCheckSeconds = 30;
+            configurationOptions.AbortOnConnectFail = false;
+
+            var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync(configurationOptions);
+            return connectionMultiplexer.GetDatabase(0);
+        }
+
+        public async Task Run()
+        {
+            var requestInterval = TimeSpan.FromMilliseconds(500);
+
+            while (true)
+            {
+                var setResult = _RedisDatabase.StringSet("test-key", "test-value");
+                Console.WriteLine(setResult);
+                var getResult = _RedisDatabase.StringGet("test-key");
+                Console.WriteLine(getResult.ToString());
+
+                await Task.Delay(requestInterval);
+            }
+        }
+    }
+}
